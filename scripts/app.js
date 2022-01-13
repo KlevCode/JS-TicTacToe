3f90b2cbfc635ab92aee22f730a902d049bad90c
KlevCode <appris@gmx.net>
2022-01-13 21:21:19 +0100

    index.html

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,9 @@
-const playerConfigOverlayElement = document.getElementsById('config-overlay');
+const playerConfigOverlayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
+const formElement = document.querySelector('form');
 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
-const editPlayer2BtnElement = document.getElementsById('edit-player-2-btn');
+const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
 const cancelConfigBtnElement = document.getElementById('cancel-config-btn');
 
 editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
@@ -10,3 +11,5 @@ editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
 
 cancelConfigBtnElement.addEventListener('click', closePlayerConfig);
 backdropElement.addEventListener('click', closePlayerConfig);
+
+formElement.addEventListener('submit', savePlayerConfig);
