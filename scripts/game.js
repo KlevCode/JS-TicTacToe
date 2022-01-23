7b92158efce61f8948f45fdc22cf2bfc9a0f7856
KlevCode <appris@gmx.net>
2022-01-23 21:08:49 +0100

    scripts/

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,29 @@
+function resetGameStatus() {
+  activePlayer = 0;
+  currentRound = 1;
+  gameIsOver = false;
+  gameOverElement.firstElementChild.innerHTML = 'Du hast gewonnen, <span id="winner-name">PLAYER NAME</span>!';
+  gameOverElement.style.display = 'none';
+
+  let gameBoardIndex = 0;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      gameData[i][j] = 0;
+      const gameBoardItemElement = gameBoardItemElement.children[gameBoardIndex];
+      gameBoardElement.children[gameBoardIndex].textContent = '';
+      gameBoardItemElement.classList.remove('disabled');
+      gameBoardIndex++;
+    }
+  }
+}
+
 function startNewGame() {
-  if (players[0].name === '' || players[1].name === ''){
+  if (players[0].name === '' || players[1].name === '') {
     alert('Bitte Namen für beide Spieler eingeben!');
     return;
   }
+
+  resetGameStatus();
 
   activePlayerNameElement.textContent = players[activePlayer].name;
   gameAreaElement.style.display = 'block';
@@ -18,7 +39,7 @@ function switchPlayer() {
 }
 
 function selectGameField(event) {
-  if (event.target.tagName !== 'LI') {
+  if (event.target.tagName !== 'LI' || gameIsOver) {
     return;
   }
 
@@ -38,7 +59,10 @@ function selectGameField(event) {
   gameData[selectedRow][selectedColumn] = activePlayer + 1;
 
   const winnerId = checkForGameOver();
-  console.log(winnerId);
+
+  if (winnerId !== 0) {
+    endGame(winnerId);
+  }
 
   currentRound++;
   switchPlayer();
@@ -58,8 +82,8 @@ function checkForGameOver() {
   for (let i = 0; i < 3; i++) {
     if (
       gameData[0][i] > 0 &&
-      gameData[0][i] === gameData[0][i] &&
-      gameData[0][i] === gameData[0][i]
+      gameData[0][i] === gameData[1][i] &&
+      gameData[0][i] === gameData[2][i]
       ) {
       return gameData[0][i];
       }
@@ -85,4 +109,17 @@ function checkForGameOver() {
     return -1;
   }
   return 0;
+}
+
+function endGame(winnerId) {
+  gameIsOver = true;
+  gameOverElement.style.display = 'block';
+
+  if (winnerId > 0) {
+    const winnerName = players[winnerId - 1].name;
+    gameOverElement.firstElementChild.firstElementChild.textContent = winnerName;
+  } else {
+    gameOverElement.firstElementChild.textContent = 'Unentschieden!'
+  }
+
 }
